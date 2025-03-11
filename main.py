ef5172c6a21d66afb11ed36d9f260ee085a4b0eb
camchinny <camichinny@gmail.com>
2025-03-11 19:38:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import random
+import cv2
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import models, layers
+import albumentations as A
 
+import kagglehub
+from pathlib import Path
+
+# Download latest version
+path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
