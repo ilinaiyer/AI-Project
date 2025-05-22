89e9520b1dfe20149da10775f9b34f1ec1d75bee
camchi25 <cameronchin2025@gmail.com>
2025-05-22 19:01:27 -0400

    Update manual.py

--- manual.py
+++ manual.py
@@ -1,6 +1,3 @@
-#%%writefile streamlit_app/app.py
-
-# Import libraries
 import pandas as pd
 import numpy as np
 import pickle
@@ -10,12 +7,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-
-# ----- SECTION 2: CREATE THE STREAMLIT APP -----
-
-# Create a directory for the Streamlit app
-#!mkdir -p streamlit_app
-# use "python -m streamlit run app.py" if streamlit not running through terminal
 
 def app():
 
@@ -118,23 +109,3 @@ def app():
     st.subheader('Prediction Probability:')
     prob_df = pd.DataFrame(prediction_proba.detach().numpy(), columns=target_names)
     st.write(prob_df)
-
-    # Add a sample image based on prediction
-    #st.subheader('Iris Type:')
-    #if prediction[0] == 0:
-    #    st.write('Iris Setosa')
-    #    st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg/440px-Kosaciec_szczecinkowaty_Iris_setosa.jpg', width=300)
-    #elif prediction[0] == 1:
-    #    st.write('Iris Versicolor')
-    #    st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Iris_versicolor_3.jpg/440px-Iris_versicolor_3.jpg', width=300)
-    #else:
-    #    st.write('Iris Virginica')
-    #    st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Iris_virginica.jpg/440px-Iris_virginica.jpg', width=300)
-
-    # ----- SECTION 3: RUN THE STREAMLIT APP IN COLAB -----
-    import os
-
-    #pip3 install -q streamlit
-    #!npm install localtunnel
-    #!wget -q -O - ipv4.icanhazip.com # USE THIS OUTPUT (ex., '34.133.84.111') as the Tunnel Password if you are asked on the next page
-    #!streamlit run streamlit_app/app.py & npx localtunnel --port 8501
